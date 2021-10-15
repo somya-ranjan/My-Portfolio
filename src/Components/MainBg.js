fe933ef56f34b6e626ef17fb133f746dab195cd3
Somyaranjan <mr.somyars@gmail.com>
2021-10-15 15:10:36 +0530

    main_bg_git&linkedIn_add

--- src/Components/MainBg.js
+++ src/Components/MainBg.js
@@ -7,7 +7,7 @@ function MainBg() {
     return (
         <div className="container-fluid mainBgSection" id="home">
             <div className="row justify-content-evenly align-items-center mainBgContent">
-                <div data-aos="slide-right"  className="col-10 textAnimation">
+                <div data-aos="slide-right" className="col-10 textAnimation">
                     <h1>Hi...</h1>
                     <h2>
                         <Typical
@@ -20,10 +20,20 @@ function MainBg() {
                             ]}
                         />
                     </h2>
-                    <button> <a href="https://drive.google.com/file/d/1AV3tK-U76pHoZa5lH6ykzRAHc3D78hkW/view?usp=sharing" target="_blank" rel="noopener noreferrer">My Resume</a> </button>
+                    <button> <a href="https://drive.google.com/file/d/11z0RRY_8NCNMf8EQ73DLx-mPzJAGrS32/view?usp=sharing" target="_blank" rel="noopener noreferrer">My Resume</a> </button>
+
+                    <span>
+                        <a href="https://github.com/somya-ranjan" target="_blank" rel="noopener noreferrer" className="gitHub">
+                            <i class="fab fa-github"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/somya-ranjan/" target="_blank" rel="noopener noreferrer" className="linkedIn">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </span>
+                    
                 </div>
                 <div data-aos="slide-left" className="col-6 mainBgImg">
-                    <img src="media/my.png" alt="" className="img-fluid"/>
+                    <img src="media/my.png" alt="" className="img-fluid" />
                 </div>
             </div>
         </div>
